9933118dbf78aa918ff6e7d694460861529b7f76
preciousyaks17 <89774835+preciousyaks17@users.noreply.github.com>
2022-06-22 03:41:42 +0100

    javasript code that prints my name, height and country on the screen - first deployment

---
+++ script.js
@@ -0,0 +1,6 @@
+const myName = "Precious Yakubu";
+const myHeight = "167.5cm";
+const myCountry = "Nigeria";
+alert("My name is " + myName);
+alert("My height is" + myHeight);
+alert("The name of my country is " + myCountry);
